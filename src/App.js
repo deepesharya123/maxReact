cadc333c82782f590497e29c0e3671b043da01dc
deepesharya123 <deepesharya82246@gmail.com>
2022-01-16 02:03:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-function App() {
-  const para = document.createElement("p");
-  para.textContent = "This will also be visible";
-  document.getElementById("root").appendChild(para);
+import ExpenseItem  from "./components/ExpenseItem";
 
+function App() {
+  // this is imparative approach as i am tellling line by line what to do to render the html.
+  // const para = document.createElement("p");
+  // para.textContent = "This will also be visible";
+  // document.getElementById("root").appendChild(para);
+
+  // below is declarative approach 
   return (
     <div>
       <h1>Let's get started.</h1>
-      <p>This is also visible.</p>  
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
