e2892d447ab145f1388657bfae195e3b2cce2143
deepesharya123 <deepesharya82246@gmail.com>
2022-01-17 13:08:23 +0530

    adding more component and css files

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
-function ExpenseItem(){
-    return <h2>Expense Item file !</h2>
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>
+        <ExpenseDate date={props.date}/>
+      </div>
+      <div className="expense-item__description">
+        <h2> {props.title}</h2>
+        <div className="expense-item__price">Rs {props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
