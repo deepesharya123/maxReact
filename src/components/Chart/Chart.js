fbff6649f550b25f9440fe3b9d58aca6d9f84bfa
deepesharya123 <deepesharya82246@gmail.com>
2022-01-23 16:35:33 +0530

    showing char

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,19 +4,19 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
-    const dataPointValues = props.dataPoints.map((datapoint) => datapoint.value);
-    const totalMaximum = Math.max(...dataPointValues);
+  const dataPointValues = props.dataPoints.map((datapoint) => datapoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
 
   return (
     <div className="chart">
-      {props.dataPoints.map((dataPoint) => {
+      {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
           maxValue={totalMaximum}
           label={dataPoint.label}
-        />;
-      })}
+        />
+      ))}
     </div>
   );
 };
