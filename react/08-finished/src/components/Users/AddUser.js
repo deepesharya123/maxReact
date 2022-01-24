bb9a0cce410782bc719b5f7cf3cc06a53bb399af
deepesharya123 <deepesharya82246@gmail.com>
2022-01-25 00:30:40 +0530

    change

--- react/08-finished/src/components/Users/AddUser.js
+++ react/08-finished/src/components/Users/AddUser.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
-import Wrapper from './components/Helper/Wrapper';
+import Wrapper from '../Helper/Wrapper'
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
