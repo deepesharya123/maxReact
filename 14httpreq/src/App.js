670b550b96d41c67b83db59d54e088dbc032f937
deepesharya123 <deepesharya82246@gmail.com>
2022-02-03 15:17:43 +0530

    onLoading change

--- 14httpreq/src/App.js
+++ 14httpreq/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useCallback, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -26,7 +26,7 @@ function App() {
   //   })
   // }
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async function () {
     try {
       setIsLoading(true);
       setError(null);
@@ -50,7 +50,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  },[]);
+
+  useEffect(()=>{
+    fetchMoviesHandler()
+  },[fetchMoviesHandler]);
 
   let content = <p>Found no movies</p>;
 
